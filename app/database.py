45a6a34fa4a2c423a127a3fd46903aa72a7be9b5
adaboubvincent <adaboubvincent@gmail.com>
2020-09-29 23:16:29 +0200

    Modifiy file

--- app/database.py
+++ app/database.py
@@ -5,10 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/fastapi"
-
+#SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/fastapi"
+DATABASE_URL ="postgres://hmzfwjaablpcld:ad0854bd76281509931756b9bf31bc196b971c7259e8acf99190d7779f7c4d9d@ec2-34-238-26-109.compute-1.amazonaws.com:5432/db92ma3odp73g6"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    DATABASE_URL,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
